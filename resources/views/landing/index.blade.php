331f5ee3851b5767a673efd8041fc59853a64a79
JustMorron <aliefandika30@gmail.com>
2025-11-23 20:52:47 +0700

    first commit

---
+++ resources/views/landing/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    {{-- Hero --}}
+    <section class="text-center py-20 bg-white">
+        <h1 class="text-5xl font-bold">Selamat Datang</h1>
+        <p class="mt-4 text-lg">Ini adalah landing page Laravel pertama kamu.</p>
+        <a href="#" class="mt-6 inline-block bg-blue-600 text-white px-6 py-3 rounded-lg">
+            Mulai Sekarang
+        </a>
+    </section>
+@endsection
